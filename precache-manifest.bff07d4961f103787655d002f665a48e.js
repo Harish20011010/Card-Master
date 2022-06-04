131dd4fbcaaa58dcc2b28a8afce632311190360e
Harish20011010 <harrish2137@gmail.com>
2022-06-05 01:55:27 +0530

    Updates

---
+++ precache-manifest.bff07d4961f103787655d002f665a48e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7f118a02e99a8ac49a49d69e0dc170",
+    "url": "/Card-Master/index.html"
+  },
+  {
+    "revision": "e300271ed972f8be41ec",
+    "url": "/Card-Master/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1aa11949a9e72dca1f84",
+    "url": "/Card-Master/static/css/main.1a5150fb.chunk.css"
+  },
+  {
+    "revision": "e300271ed972f8be41ec",
+    "url": "/Card-Master/static/js/2.fa394f24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Card-Master/static/js/2.fa394f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa11949a9e72dca1f84",
+    "url": "/Card-Master/static/js/main.3a9d921e.chunk.js"
+  },
+  {
+    "revision": "57382c1488d89101db62",
+    "url": "/Card-Master/static/js/runtime-main.7107ce53.js"
+  },
+  {
+    "revision": "fb08f8fed4141719402d79073c478131",
+    "url": "/Card-Master/static/media/cart.fb08f8fe.svg"
+  }
+]);
